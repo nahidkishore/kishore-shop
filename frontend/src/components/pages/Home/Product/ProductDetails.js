0bfe53f1ac9315c2cbde336c2e11feb2d22e2216
nahid <nahidkishore99@gmail.com>
2021-01-19 23:24:00 +0600

    product details added

--- frontend/src/components/pages/Home/Product/ProductDetails.js
+++ frontend/src/components/pages/Home/Product/ProductDetails.js
@@ -1,10 +1,70 @@
 import React from 'react';
+import { Button, Card, Col, Image, ListGroup, Row } from 'react-bootstrap';
+import { Link, useParams } from 'react-router-dom';
+import products from '../../../../products';
+import Rating from '../Rating';
 
-const ProductDetails = () => {
+const ProductDetails = ({ match }) => {
+  const { id } = useParams();
+  const product = products.find((p) => p._id === id);
+  console.log(product);
   return (
-    <div>
-      <h2>Product details here</h2>
-    </div>
+    <>
+      <Link className='btn btn-light my-3' to='/'>
+        Go Back
+      </Link>
+
+      <Row>
+        <Col md={6}>
+          <Image
+            src={product.image}
+            alt={product.name}
+            fluid
+            style={{ width: '25em' }}
+          />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h2>{product.name}</h2>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews}reviews`}
+              ></Rating>
+            </ListGroup.Item>
+            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+            <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col>
+                  Price:
+                  </Col>
+                  <Col><strong>{product.price}</strong></Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>
+                  Status:
+                  </Col>
+                  <Col>{product.countInStock>0?'In Stock':'Out OF Stock'}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button className="btn-block" type="button" disabled={product.countInStock===0}>Add To Cart</Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
   );
 };
 
