1ee223b1ece6076ee4b6e03b5e021a7758ec1a00
nahid <nahidkishore99@gmail.com>
2021-01-20 14:49:12 +0600

    connect to mongoDB database and compass

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+const connectDB=async () => {
+  try {
+    const conn=await mongoose.connect(process.env.MONGO_URI,{
+       useUnifiedTopology:true,
+       useNewUrlParser:true,
+       useCreateIndex:true
+    })
+    console.log(`MongoDB connected: ${conn.connection.host}`)
+  } catch (error) {
+    console.log(`Error: ${error.message}`)
+    process.exit(1)
+  }
+}
+export default connectDB
