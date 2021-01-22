b16ffe0a22d670bf67c164dd2f8a2fb88fc60eb2
nahid <nahidkishore99@gmail.com>
2021-01-22 23:28:19 +0600

    laoder and aler message component added

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -12,7 +12,9 @@ router.get(
   '/',
   asyncHandler(async (req, res) => {
     const products = await Product.find({});
+ 
     res.json(products);
+   
   })
 );
 
--- frontend/src/components/pages/Home/Home.js
+++ frontend/src/components/pages/Home/Home.js
@@ -1,27 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Col, Row } from 'react-bootstrap';
-import {useDispatch, useSelector} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Product from './Product/Product';
-import {listProducts} from '../../../actions/productActions'
+import { listProducts } from '../../../actions/productActions';
+import Loading from '../LoadingAndMessage/Loader';
+import Message from '../LoadingAndMessage/Message';
+
 const Home = () => {
-const dispatch=useDispatch()
-const productList =useSelector(state=>state.productList)
-const {loading,error,products}=productList
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
   useEffect(() => {
-    dispatch(listProducts())
+    dispatch(listProducts());
   }, [dispatch]);
-  
+
   return (
     <>
       <h1>Latest products list here </h1>
-      {loading?<h2>Loading... </h2>:error? <h3>{error}</h3>:   <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4}>
-            <Product product={product}></Product>
-          </Col>
-        ))}
-      </Row>}
-   
+      {loading ? (
+        <Loading />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4}>
+              <Product product={product}></Product>
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
