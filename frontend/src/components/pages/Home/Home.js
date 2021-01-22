4739c55b49dd2591f3d122b449798466e0a4e402
nahid <nahidkishore99@gmail.com>
2021-01-22 22:49:12 +0600

    useDispatch and useSelector for get products replace of useState hooks

--- frontend/src/components/pages/Home/Home.js
+++ frontend/src/components/pages/Home/Home.js
@@ -1,27 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import { Col, Row } from 'react-bootstrap';
-
+import {useDispatch, useSelector} from 'react-redux';
 import Product from './Product/Product';
-import axios from 'axios';
+import {listProducts} from '../../../actions/productActions'
 const Home = () => {
-  const [products, setProducts] = useState([]);
+const dispatch=useDispatch()
+const productList =useSelector(state=>state.productList)
+const {loading,error,products}=productList
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products');
-      setProducts(data);
-    };
-    fetchProducts();
-  }, []);
+    dispatch(listProducts())
+  }, [dispatch]);
+  
   return (
     <>
       <h1>Latest products list here </h1>
-      <Row>
+      {loading?<h2>Loading... </h2>:error? <h3>{error}</h3>:   <Row>
         {products.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={4}>
             <Product product={product}></Product>
           </Col>
         ))}
-      </Row>
+      </Row>}
+   
     </>
   );
 };
