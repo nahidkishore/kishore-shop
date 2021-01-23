083789fcb115199aa7894c722a4dee31f4a463e2
nahid <nahidkishore99@gmail.com>
2021-01-23 22:09:04 +0600

    route controller clean up

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,38 +1,12 @@
 import express from 'express';
-import asyncHandler from 'express-async-handler';
 const router = express.Router();
 
-import Product from '../models/productModel.js';
+import {
+  getProducts,
+  getProductById,
+} from '../controllers/productController.js';
 
-//@desc.... Fetch all products
-//@route ... Get/api/products
-// @access ....public
-
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
- 
-    res.json(products);
-   
-  })
-);
-
-//@desc.... Fetch single products
-//@route ... Get/api/products/:id
-// @access ....public
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404)
-      throw new Error('Product not found')
-    }
-  })
-);
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 export default router;
