a5d47d945fbc51425424df2721871be760dfe7ca
nahid <nahidkishore99@gmail.com>
2021-01-24 01:13:53 +0600

    user authentication

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs'
 const userSchema = mongoose.Schema(
   {
     name: {
@@ -23,6 +24,10 @@ const userSchema = mongoose.Schema(
   { timestamps: true }
 );
 
+
+userSchema.methods.matchPassword=async function(enterPassword){
+  return await bcrypt.compare(enterPassword,this.password)
+}
 const User = mongoose.model('User', userSchema);
 
 export default User;
--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,32 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
-import {notFound,errorHandler} from './middleware/errorMiddleware.js'
-import connectDB from './config/db.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import connectDB from './config/db.js';
 
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js'
 dotenv.config();
 
-connectDB()
+connectDB();
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('<h2>server is running bro</h2>');
 });
 
-app.use('/api/products',productRoutes)
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
-app.use(notFound)
+app.use(notFound);
 
-
-app.use(errorHandler)
+app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`.green.bold)
+  console.log(
+    `server running in ${process.env.NODE_ENV} mode on port ${PORT}`.green.bold
+  )
 );
