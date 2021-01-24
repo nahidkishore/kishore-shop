7cc8e4bd9f661244aeca801efcfeef1a6878c09b
nahid <nahidkishore99@gmail.com>
2021-01-24 22:03:15 +0600

    user login screen and functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,22 +9,28 @@ import {
 } from "react-router-dom";
 import ProductDetails from './components/pages/Home/Product/ProductDetails';
 import Cart from './components/pages/Cart/Cart';
+import Login from './components/pages/Login/Login';
+
 const App = () => {
   return (
     <Router>
       <Header></Header>
       <main className='py-3'>
         <Container>
-       
-         <Route exact path="/" >
-           <Home></Home>
-         </Route>
+       <Route path='/login'>
+         <Login></Login>
+
+       </Route>
+        
          <Route path="/product/:id">
            <ProductDetails></ProductDetails>
          </Route>
          
          <Route path="/cart/:id?"> 
            <Cart></Cart>
+         </Route>
+         <Route exact path="/" >
+           <Home></Home>
          </Route>
         </Container>
       </main>
---
+++ frontend/src/components/pages/Login/Login.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from 'react';
+import { Button, Col, Form, Row } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { Link, useHistory, useLocation } from 'react-router-dom';
+import { login } from '../../../actions/userActions';
+import FormContainer from '../FormContainer';
+import Message from '../LoadingAndMessage/Message';
+import Loading from '../LoadingAndMessage/Loader';
+
+const Login = () => {
+  const history = useHistory();
+  const location = useLocation();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+  const userLogin = useSelector((state) => state.userLogin);
+  const { loading, error, userInfo } = userLogin;
+
+  const redirect = location.search ? location.search.split('=')[1] : '/';
+
+  useEffect(() => {
+    if (userInfo) {
+      history.push(redirect);
+    }
+  }, [history, userInfo, redirect]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    //Dispatch login
+
+    dispatch(login(email, password));
+  };
+  return (
+    <FormContainer>
+      <h1>Sign In</h1>
+      {error && <Message variant='danger'>{error}</Message>}
+      {loading && <Loading></Loading>}
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId='emails'>
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Enter your email address'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group controlId='password'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Enter your  password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Button type='submit' variant='primary'>
+          Sign In
+        </Button>
+      </Form>
+
+      <Row>
+        <Col>
+          New Customer?{' '}
+          <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>
+            Register
+          </Link>
+        </Col>
+      </Row>
+    </FormContainer>
+  );
+};
+
+export default Login;
