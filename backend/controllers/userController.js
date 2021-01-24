9348a191221417ca1a2fd39eef93a6ce80304562
nahid <nahidkishore99@gmail.com>
2021-01-24 18:20:08 +0600

    custom authentication middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,21 +7,38 @@ import User from '../models/userModel.js';
 // @access ....public
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
-const user=await User.findOne({email})
-if(user && (await user.matchPassword(password))){
-res.json({
-
-  _id: user._id,
-  name: user.name,
-  email: user.email,
-  isAdmin: user.isAdmin,
-  token:generateToken(user._id)
-})
-}
-else{
-  res.status(401)
-  throw new Error('Invalid email and password')
-}
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email and password');
+  }
 });
 
-export { authUser };
+//@desc.... GET user profile
+//@route ... GET /api/users/profile
+// @access ....private
+
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+export { authUser, getUserProfile };
