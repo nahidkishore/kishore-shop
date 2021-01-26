4eb5853d2bb0871be8a7504080187476b9fd29b0
nahid <nahidkishore99@gmail.com>
2021-01-26 23:51:05 +0600

    create order by id

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,11 +2,12 @@ import express from 'express';
 const router = express.Router();
 
 import {
- addOrderItems
+ addOrderItems,getOrderById
 } from '../controllers/orderController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 router.route('/').post(protect,addOrderItems);
+router.route('/:id').get(protect,getOrderById)
 
 
 
