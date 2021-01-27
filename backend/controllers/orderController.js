e80b0ff3d780e546f2fd91299c55246bd41b25fd
nahid <nahidkishore99@gmail.com>
2021-01-28 00:29:22 +0600

    show orders in user profile section

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -72,5 +72,13 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
     throw new Error('Order not found');
   }
 });
+// @desc    Get logged in user orders
+// @route   GET /api/orders/myorders
+// @access  private
 
-export { addOrderItems, getOrderById ,updateOrderToPaid};
+const getMyOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({ user: req.user._id });
+  res.json(orders);
+});
+
+export { addOrderItems, getOrderById ,updateOrderToPaid,getMyOrders};
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,13 +2,17 @@ import express from 'express';
 const router = express.Router();
 
 import {
- addOrderItems,getOrderById, updateOrderToPaid
+  addOrderItems,
+  getMyOrders,
+  getOrderById,
+  updateOrderToPaid,
 } from '../controllers/orderController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
-router.route('/').post(protect,addOrderItems);
-router.route('/:id').get(protect,getOrderById)
-router.route('/:id/pay').put(protect,updateOrderToPaid)
+router.route('/').post(protect, addOrderItems);
 
+router.route('/myorders').get(protect, getMyOrders);
+router.route('/:id').get(protect, getOrderById);
+router.route('/:id/pay').put(protect, updateOrderToPaid);
 
 export default router;
