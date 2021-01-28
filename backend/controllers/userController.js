12a4c5119be4fac55b5a30afbfd4896121266ce9
nahid <nahidkishore99@gmail.com>
2021-01-28 11:43:29 +0600

    admin middleware and admin route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -92,4 +92,19 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile, registerUser,updateUserProfile };
+
+//@desc.... GET all user
+//@route ... GET /api/users/
+// @access ....private/admin
+
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+  res.json(users)
+ 
+});
+
+
+
+
+
+export { authUser, getUserProfile, registerUser,updateUserProfile,getUsers };
