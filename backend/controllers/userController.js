fb78b44d2bae6e5de51f0bfe21cc646e6b204299
nahid <nahidkishore99@gmail.com>
2021-01-29 16:08:02 +0600

    get user by id and updated user user by id in backend side  only access  for admin

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -116,6 +116,48 @@ const deleteUser = asyncHandler(async (req, res) => {
   }
 });
 
+//@desc.... GET user by ID 
+//@route ... GET /api/users/:id
+// @access ....private/admin
+
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password');
+ 
+ if(user){
+  res.json(user);
+ }
+ else {
+  res.status(404);
+  throw new Error('User not found');
+}
+});
+
+//@desc.... Update user 
+//@route ... PUT /api/users/:id
+// @access ....private/admin
+
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+   user.isAdmin = req.body.isAdmin || user.isAdmin;
+
+    const updateUser = await user.save();
+    res.json({
+      _id: updateUser._id,
+      name: updateUser.name,
+      email: updateUser.email,
+      isAdmin: updateUser.isAdmin,
+  
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+
 export {
   authUser,
   getUserProfile,
@@ -123,4 +165,6 @@ export {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
+  updateUser
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -8,14 +8,20 @@ import {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
+  updateUser,
 } from '../controllers/userController.js';
-import { protect,admin } from '../middleware/authMiddleware.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
-router.route('/').post(registerUser).get(protect,admin,getUsers);
+router.route('/').post(registerUser).get(protect, admin, getUsers);
 router.post('/login', authUser);
 router
   .route('/profile')
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile);
-router.route('/:id').delete(protect,admin,deleteUser);
+router
+  .route('/:id')
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser);
 export default router;
