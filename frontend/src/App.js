2b386db0025bc9189d3a74e21e54ef8a8f548a50
nahid <nahidkishore99@gmail.com>
2021-01-29 19:21:52 +0600

    product list added in admin pannel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import UserListScreen from './screens/UserListScreen';
 import UserEditScreen from './screens/UserEditScreen';
+import ProductListScreen from './screens/ProductListScreen';
 
 
 const App = () => {
@@ -58,6 +59,9 @@ const App = () => {
           </Route>
           <Route path='/admin/user/:id/edit'>
             <UserEditScreen></UserEditScreen>
+          </Route>
+          <Route path='/admin/productlist'>
+            <ProductListScreen/>
           </Route>
           <Route exact path='/'>
             <Home></Home>
