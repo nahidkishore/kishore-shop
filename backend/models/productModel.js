10a7ff6bcf998c69d55d9eef9af78f1cee0d9132
nahid <nahidkishore99@gmail.com>
2021-01-30 19:50:10 +0600

    customer review working in backend

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -7,6 +7,11 @@ const reviewSchema = mongoose.Schema(
     name: { type: String, required: true },
     rating: { type: Number, required: true },
     comment: { type: String, required: true },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
   },
   { timestamps: true }
 );
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -8,9 +8,11 @@ import {
   deleteProduct,
   updateProduct,
   createProduct,
+  createProductReview,
 } from '../controllers/productController.js';
 
 router.route('/').get(getProducts).post(protect, admin, createProduct);
+router.route('/:id/reviews').post(protect, createProductReview);
 router
   .route('/:id')
   .get(getProductById)
