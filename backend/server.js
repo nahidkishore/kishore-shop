3b5e4f94bc91cb7214b8785828952e0bb4f8503c
nahid <nahidkishore99@gmail.com>
2021-01-31 18:41:18 +0600

    updated

--- backend/server.js
+++ backend/server.js
@@ -15,13 +15,11 @@ dotenv.config();
 connectDB();
 const app = express();
 
-if (process.env.NODE_ENV === 'developement') {
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
 app.use(express.json());
-
-
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
@@ -35,13 +33,12 @@ app.get('/api/config/paypal', (req, res) =>
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-
-if(process.env.NODE_ENV==='production'){
-  app.use(express.static(path.join(__dirname,'/frontend/build')))
-  app.get('*',(req, res)=>
-  res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
-  )
-} else{
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
   app.get('/', (req, res) => {
     res.send('<h2>server is running bro</h2>');
   });
