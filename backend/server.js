a246afd3162bc058c572ba7f47c20be23324ffe4
nahid <nahidkishore99@gmail.com>
2021-01-31 19:44:31 +0600

    updated

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,10 @@ dotenv.config();
 connectDB();
 const app = express();
 
-if (process.env.NODE_ENV !== 'production') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
 }
+
 
 app.use(express.json());
 
@@ -41,7 +42,7 @@ if (process.env.NODE_ENV === 'production') {
   );
 } else {
   app.get('/', (req, res) => {
-    res.send('Server is running Bro...');
+    res.send('<h2>server is running bro</h2>');
   });
 }
 
